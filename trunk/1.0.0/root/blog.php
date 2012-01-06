cb5e5da7564f0b0ced8105abbbe3ae92fd61091a
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-01-06 01:19:21 +0000

    Thats why you don't code at 1AM. You forget commas and semi-colons.

--- trunk/1.0.0/root/blog.php
+++ trunk/1.0.0/root/blog.php
@@ -34,7 +34,7 @@ $blog_id	= request_var('id', '');
 $cat_id		= request_var('cid', '');
 $blog 		= new blog;
 $redirect	= request_var('redirect', '');
-$blog_on_c	= $config['blog_on']
+$blog_on_c	= $config['blog_on'];
 $act_name 	= $config['blog_act_name'];
 $action		= request_var($act_name, 'index');
 $manager 	= ($auth->acl_get('a_blog_manage') == 1) ? 1 : 0;
@@ -47,7 +47,7 @@ $template->assign_vars(array(
 	'S_NEW_POST'	 		=> ($auth->acl_get('u_blog_post')) ? true : false,
 	'ACT_NAME'   			=> $act_name,
 	'U_BLOG_MANAGE'			=> $manager,
-	'BLOG_ENABLED_C'		=> $blog_on_c
+	'BLOG_ENABLED_C'		=> $blog_on_c,
 	'U_BLOG_INDEX'			=> append_sid($phpbb_root_path . 'blog.' . $phpEx),
 	'NEW_POST'				=> $user->img('button_article_new', 'New Blog'),
 	'OVERALL_BLOG_DESC'		=> $config['blog_description'],
