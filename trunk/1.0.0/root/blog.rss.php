0a8874bae0cde290b68cb4b94469cf664c65ed80
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-01-07 10:27:37 +0000

    Update Version Number

--- trunk/1.0.0/root/blog.rss.php
+++ trunk/1.0.0/root/blog.rss.php
@@ -3,7 +3,7 @@
 * Main file
 *
 * @package Blog RSS Feed
-* @version 1.0.0
+* @version 1.0.1
 * @copyright (c) 2010, 2011 Michael Cullum (Unknown Bliss of http://michaelcullum.com)
 * @license http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
 *
