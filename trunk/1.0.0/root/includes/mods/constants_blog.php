a2be66f4cc77461ecebccd6e4cab5ed35147a828
Erik Frèrejean <erikfrerejean@phpbb.com>
2012-01-10 18:03:38 +0800

    Begin fix #59, get rid of hardcoded table names in the installer

---
+++ trunk/1.0.0/root/includes/mods/constants_blog.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Constants
+ *
+ * @package Blog
+ * @version 1.0.1
+ * @copyright (c) 2010, 2011 Michael Cullum (Unknown Bliss of http://michaelcullum.com)
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
+ *
+ */
+
+define('BLOGS_TABLE',		$table_prefix . 'blog');
+define('BLOG_CMNTS_TABLE',	$table_prefix . 'blog_comments');
+define('BLOG_CATS_TABLE',	$table_prefix . 'blog_categories');
